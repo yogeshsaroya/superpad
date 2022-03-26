47676230f3c4717cd2ac0164cfc91806b2f983ae
Yogesh Saroya <yogeshsaroya@gmail.com>
2022-03-26 10:52:42 +0530

    update team #2

--- src/Controller/HomesController.php
+++ src/Controller/HomesController.php
@@ -90,7 +90,7 @@ class HomesController extends AppController
     public function team()
     {
                 
-        $query = $this->Teams->find('all', ['conditions' => ['Teams.status'=>1],'limit' => 100,'order'=>['Teams.id'=>'DESC']]);
+        $query = $this->Teams->find('all', ['conditions' => ['Teams.status'=>1],'limit' => 100,'order'=>['Teams.title'=>'DESC']]);
         $data = $query->all();
         $this->set(compact('data'));
     }
