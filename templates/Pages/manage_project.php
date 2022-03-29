22e5a4ce48618ccd683b24266000c0367872e84c
Yogesh Saroya <yogeshsaroya@gmail.com>
2022-03-29 14:32:05 +0530

    update heading

--- templates/Pages/manage_project.php
+++ templates/Pages/manage_project.php
@@ -18,7 +18,7 @@ $this->assign('title', 'Manage Projects'); ?>
             <div class="content-header-left col-md-9 col-12 mb-2">
                 <div class="row breadcrumbs-top">
                     <div class="col-12">
-                        <h2 class="content-header-title float-left mb-0">Manage Projects</h2>
+                        <h2 class="content-header-title float-left mb-0"><?php echo (isset($get_data->title) ? $get_data->title :  "Add New Project" ) ?> </h2>
                     </div>
                 </div>
             </div>
